f9b61ca34f71816acc1fa046d7a96288a4c70fc6
Naresh Babu <jeenepallinaresh429@gmail.com>
2026-01-02 19:58:00 +0530

    Shell scriptinh Practice

--- 13-Scripting.sh
+++ 13-Scripting.sh
@@ -3,12 +3,14 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
     echo "ERROR :: This script should run with root privileges"
+    exit 1
 fi
 
 dnf install mysql -y
 
 if [ $? -ne 0 ]; then
     echo "MySQL installation is failed"
+    exit 1
 else
     echo "Installing MySql is SUCCESS"
 fi
