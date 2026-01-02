e14f02efaf516da0c9568b54528e0045ad8caaa2
Naresh Babu <jeenepallinaresh429@gmail.com>
2026-01-02 18:25:58 +0530

    Shell scriptinh Practice

--- 10-datatypes.sh
+++ 10-datatypes.sh
@@ -5,7 +5,7 @@ SUM=$(($NUMBER1+$NUMBER2))
 
 echo "Sum of the above 2 numbers is :: ${SUM}"
 
-leads= ("Modi" "Putin" "Rahul" "Trump")
+leads=("Modi" "Putin" "Rahul" "Trump")
 
 echo "Print all of the leaders at a time : ${leads[@]}" 
 echo "Print all of the leaders at a time : ${leads[0]}"
