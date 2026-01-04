8e63372b0b42f77fc4f92864ecfada949c085cd9
Naresh Babu <jeenepallinaresh429@gmail.com>
2026-01-04 18:11:39 +0530

    Shell scriptinh Practice

--- 19-whileLoops.sh
+++ 19-whileLoops.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-count=5
+# count=5
 
-while [ $count -gt 0 ]
+# while [ $count -gt 0 ]
+# do
+# echo "Processing $count ..."
+# sleep 1 
+# count=$((count-1))
+# done
+# echo "Times Up!!...."
+
+
+while IFS= read -r filepath
 do
-echo "Processing $count ..."
-sleep 1 
-count=$((count-1))
-done
-echo "Times Up!!...."
+echo "$filepath"
+done > 21-script-1.sh
