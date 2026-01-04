76f0a37cc7cdb6ec2102c22b34e6665db5f53307
Naresh Babu <jeenepallinaresh429@gmail.com>
2026-01-04 18:08:07 +0530

    Shell scriptinh Practice

--- 20-set.sh
+++ 20-set.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-trap 'echo "There is an error in $LINENO, Command is: $BASH_COMMAND"' ERR
+# trap 'echo "There is an error in $LINENO, Command is: $BASH_COMMAND"' ERR
 
 echo "Hello.."
 echo "Before error.."
